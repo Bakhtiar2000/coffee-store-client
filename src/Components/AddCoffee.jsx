61a3fba68fe83c797409639e25ab1295e3356002
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-05-08 22:08:06 +0600

    added addCoffee and updateCoffee components

---
+++ src/Components/AddCoffee.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h2>Add a coffee</h2>
+            <form>
+                <div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Coffee Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" placeholder="Coffee name" className="input input-bordered" />
+                        </label>
+                    </div>
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" placeholder="Available Quantity" className="input input-bordered" />
+                        </label>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default AddCoffee;
